5142b2bd284845cfe24e89f67dcc3f1aba073e10
TMaYaD <tmayad@loonyb.in>
2009-12-14 02:06:18 +0530

    Initial support for authlogic
    Signed-off-by: TMaYaD <tmayad@loonyb.in>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resource :account, :controller => 'users'
+  map.resources :users
+
+  map.resource :user_session
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
