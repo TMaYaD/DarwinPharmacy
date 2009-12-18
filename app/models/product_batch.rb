9c00e38295ffb5726d916662723075dba5df15f6
TMaYaD <tmayad@loonyb.in>
2009-12-19 05:29:41 +0530

    Multiple items in new order
    Signed-off-by: TMaYaD <tmayad@loonyb.in>

--- app/models/product_batch.rb
+++ app/models/product_batch.rb
@@ -1,3 +1,7 @@
 class ProductBatch < ActiveRecord::Base
   belongs_to :product
+
+  def to_label
+    self.product.name + ':' + self.batch_code
+  end
 end
---
+++ public/javascripts/jquery.labelover.js
@@ -0,0 +1,35 @@
+jQuery.fn.labelOver = function(overClass) {
+    return this.each(function(){
+        var label = jQuery(this);
+        var f = label.attr('for');
+        if (f) {
+            var input = jQuery('#' + f);
+            /*
+            this.hide = function() {
+              label.css({ textIndent: -10000 })
+            }
+            
+            this.show = function() {
+              if (input.val() == '') label.css({ textIndent: 0 })
+            }
+            */
+
+            // handlers
+            input.focus(function() {
+                label.hide();
+            });
+            input.blur(function() {
+                if (input.val() == '') label.show();
+            });
+            
+            label.addClass(overClass).css({
+                'margin-right': -label.width()-4,
+                'padding-left': 4,
+                'position': 'relative',
+                'z-index': 1,
+                'cursor': 'text',
+            }).click(function(){ input.focus() });
+            if (input.val() != '') label.hide(); 
+        }
+    })
+}
