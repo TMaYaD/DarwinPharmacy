fe87879a8ff4c8ed07fc3677f210b77bd03dfbcf
TMaYaD <tmayad@loonyb.in>
2009-12-28 12:28:55 +0530

    Preliminary data tables support
    Signed-off-by: TMaYaD <tmayad@loonyb.in>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,21 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def datatable(&block)
+    concat javascript_include_tag('jquery.dataTables.min'), block.binding
+    concat stylesheet_link_tag('/themes/smoothness/jquery-ui-1.7.2.custom.css'), block.binding
+    concat "
+          <script>
+            $(document).ready(function() {
+              $('#data').dataTable({
+                'bJQueryUI': true,
+                'sPaginationType': 'full_numbers',
+                'bStateSave': true,
+              });
+            });
+          </script>", block.binding
+    concat "
+        <table id='data'>", block.binding
+    yield
+    concat "</table>", block.binding
+  end
 end
