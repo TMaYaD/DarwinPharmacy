e6de33eaa7f4d4635bbbd0ea277a79dbd064645c
TMaYaD <tmayad@loonyb.in>
2009-12-30 08:51:57 +0530

    Layout design
    Completed work on users views.
    Pending rest of the views.
    
    Signed-off-by: TMaYaD <tmayad@loonyb.in>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,8 @@ module ApplicationHelper
   def datatable(&block)
     concat javascript_include_tag('jquery.dataTables.min')
     concat stylesheet_link_tag('/themes/smoothness/jquery-ui-1.7.2.custom.css')
+#   toolbar_class = 'fg-toolbar ui-widget-header ui-corner-tl ui-corner-tr ui-helper.clearfix'
+#               'sDom': '<lfr>t<<\"ui-icon ui-icon-plusthick\">pi>',
     concat "
           <script>
             $(document).ready(function() {
@@ -11,6 +13,7 @@ module ApplicationHelper
                 'sPaginationType': 'full_numbers',
                 'bStateSave': true,
               });
+              $('#add_new').remove().prependTo('div.fg-toolbar:last');
             });
           </script>"
     concat "
