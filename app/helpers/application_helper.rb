9c8f435c6d27748908b4f9bea5abf3836034812c
TMaYaD <tmayad@loonyb.in>
2009-12-30 11:31:18 +0530

    Added a action specific js and got rid of the datatable helper function.
    changed the theme to ui-darkness
    
    Signed-off-by: TMaYaD <tmayad@loonyb.in>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,3 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def datatable(&block)
-    concat javascript_include_tag('jquery.dataTables.min')
-    concat stylesheet_link_tag('/themes/smoothness/jquery-ui-1.7.2.custom.css')
-#   toolbar_class = 'fg-toolbar ui-widget-header ui-corner-tl ui-corner-tr ui-helper.clearfix'
-#               'sDom': '<lfr>t<<\"ui-icon ui-icon-plusthick\">pi>',
-    concat "
-          <script>
-            $(document).ready(function() {
-              $('#data').dataTable({
-                'bJQueryUI': true,
-                'sPaginationType': 'full_numbers',
-                'bStateSave': true,
-              });
-              $('#add_new').remove().prependTo('div.fg-toolbar:last');
-            });
-          </script>"
-    concat "
-        <table id='data'>"
-    yield
-    concat "</table>"
-  end
 end
