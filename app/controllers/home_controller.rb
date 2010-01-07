c9c375a7ac17ba820795d0698dfa66edd61313e8
TMaYaD <tmayad@loonyb.in>
2010-01-07 13:55:37 +0530

    Supports special layout for authonticated users
    Removed login form and app navigation links from home
    fixed issue with header
    
    Signed-off-by: TMaYaD <tmayad@loonyb.in>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
+  layout :select_layout
   def index
     #render :layout => false
   end
 
+  private
+  def select_layout
+    current_user ? 'application' : 'home'
+  end
 end
