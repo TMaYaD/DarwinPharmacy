98a0b09a16f4f977c36c47eff941caf8131687cd
TMaYaD <tmayad@loonyb.in>
2010-01-07 17:16:51 +0530

    Schedule needs to be present only for schedule H drugs
    Signed-off-by: TMaYaD <tmayad@loonyb.in>

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   DOSAGE_FORMS = %w[Ampoule Capsule Cream Drops Gel Granules Injection Inhaler Liquid Lotion Ointment Powder Suspension Spray Syrup Tablets]
 
-  validates_presence_of :name, :manufacturer, :schedule, :composition, :dosage_form, :strength
+  validates_presence_of :name, :manufacturer, :composition, :dosage_form, :strength
   validates_uniqueness_of :name
   validates_inclusion_of :dosage_form, :in => DOSAGE_FORMS
 end
