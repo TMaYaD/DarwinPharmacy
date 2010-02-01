9ad2f25fb1b16f725e1b0803b13401684a1d4a69
Subhash Chandra <TMaYaD@loonyb.in>
2010-02-01 18:15:30 +0530

    Added more dosage forms
    Signed-off-by: Subhash Chandra <TMaYaD@loonyb.in>

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  DOSAGE_FORMS = %w[Ampoule Capsule Cream Drops Gel Granules Injection Inhaler Liquid Lotion Ointment Powder Soap Suspension Spray Syrup Tablets]
+  DOSAGE_FORMS = %w[Ampoule Capsule Cream Drops Gel Gelly Granules Injection Inhaler Liquid Lotion Ointment Powder Sachet Soap Suspension Spray Syrup Tablet]
 
   validates_presence_of :name, :manufacturer, :dosage_form 
   validates_uniqueness_of :name
