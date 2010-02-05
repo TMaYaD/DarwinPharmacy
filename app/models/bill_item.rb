6deb56c1bb6bfe256c536cfcc459b7cfb9abd0e1
Subhash Chandra <TMaYaD@loonyb.in>
2010-02-05 14:14:46 +0530

    Max allowed discount is 15%
    Signed-off-by: Subhash Chandra <TMaYaD@loonyb.in>

--- app/models/bill_item.rb
+++ app/models/bill_item.rb
@@ -5,7 +5,7 @@ class BillItem < ActiveRecord::Base
   #validates_presence_of :bill_id, :product_batch_id
   validates_presence_of :quantity
   validates_numericality_of :quantity, :only_integer => true, :greater_than => 0
-  validates_numericality_of :discount, :less_than => 100
+  validates_numericality_of :discount, :less_than => 15
 
   def mrp
     self.product_batch.mrp * self.quantity / self.product_batch.pack * (100 - self.discount) /100
