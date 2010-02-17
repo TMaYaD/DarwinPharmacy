9fa38825e9c3b2f007585c5d272283eada311eec
phoenix <phoenix@darkmatter.(none)>
2010-02-18 01:08:22 +0530

    Added functionality for users to edit their profiles including passwords
    Fixed permissions accordingly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.xml
   def show
-    @user = User.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @user }
@@ -26,8 +24,6 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.xml
   def new
-    @user = User.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @user }
@@ -36,18 +32,11 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    if current_user.role == 'admin' and params[:id] != 'current'
-      @user = User.find(params[:id])
-    else
-      @user = current_user
-    end
   end
 
   # POST /users
   # POST /users.xml
   def create
-    @user = User.new(params[:user])
-
     respond_to do |format|
       if @user.save
         flash[:notice] = 'User was successfully created.'
@@ -63,16 +52,16 @@ class UsersController < ApplicationController
   # PUT /users/1
   # PUT /users/1.xml
   def update
-    if current_user.role == 'admin' and params[:id] != 'current'
-      @user = User.find(params[:id])
-    else
-      @user = current_user
-    end
-
     respond_to do |format|
       if @user.update_attributes(params[:user])
         flash[:notice] = 'User was successfully updated.'
-        format.html { redirect_to(@user) }
+        format.html { 
+          if permitted_to?(:show)
+            redirect_to(@user)
+          else
+            render :action => :edit
+          end
+        }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,15 +1,21 @@
 authorization do  
   role :admin do  
     has_permission_on [:bills, :customers, :product_batches, :purchase_bills, :sale_bills, :products, :users, :franchises, :suppliers], :to => [:index, :show, :new, :create, :edit, :update, :destroy]  
-    has_permission_on :user_sessions, :to => [:destroy]
     has_permission_on [:products, :product_batches], :to => [:autocomplete]
+
+    has_permission_on :user_sessions, :to => [:destroy]
   end
 
   role :operator do
     has_permission_on [:product_batches, :products], :to => [:index, :show, :new, :create, :edit, :update]  
-    has_permission_on :user_sessions, :to => [:destroy]
     has_permission_on [:purchase_bills, :sale_bills], :to => [:index, :show, :new, :create, :edit, :update]
     has_permission_on [:products, :product_batches], :to => [:autocomplete]
+    has_permission_on :users do
+      to :update
+      if_attribute :id => is {user.id}
+    end
+
+    has_permission_on :user_sessions, :to => [:destroy]
   end
   
   role :franchise do
@@ -24,10 +30,20 @@ authorization do
       if_attribute :franchise => is {user.franchise}
     end
     has_permission_on [:products, :product_batches], :to => [:autocomplete]
+    has_permission_on :users do
+      to :update
+      if_attribute :id => is {user.id}
+    end
+
     has_permission_on :user_sessions, :to => [:destroy]
   end
   role :guest do
     has_permission_on :user_sessions, :to => [:new, :create]
   end
 end  
+privileges do
+  privilege :update do
+    includes :edit
+  end
+end
 
