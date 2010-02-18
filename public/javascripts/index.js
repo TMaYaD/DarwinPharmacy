45f0fb94560b7c472cba397c5bef9bc0bdf3f802
phoenix <phoenix@darkmatter.(none)>
2010-02-18 20:49:22 +0530

    Using tabletastic to generate index views
    changed datatables to use class for marker instead of id

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,7 @@ $.require(['jquery.dataTables.min.js', 'jquery-ui-1.7.2.custom.min.js']);
 //   toolbar_class = 'fg-toolbar ui-widget-header ui-corner-tl ui-corner-tr ui-helper.clearfix'
 //               'sDom': '<lfr>t<<\"ui-icon ui-icon-plusthick\">pi>',
 $(document).ready(function() {
-    $('#data').dataTable({
+    $('.data').dataTable({
         'bJQueryUI': true,
         'sPaginationType': 'full_numbers',
         'aoColumns': [{'sName': colName} for each (colName in sColumns)],
