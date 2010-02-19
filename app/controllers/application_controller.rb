f029da862fe111433c04221eaec35f78707bee7f
phoenix <phoenix@darkmatter.(none)>
2010-02-19 14:29:33 +0530

    moved home controller to public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def permission_denied  
     flash[:error] = "Sorry, you not allowed to access that page."  
-    redirect_to root_url  
+    redirect_to home_url  
   end  
   
   private  
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,7 +9,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_to root_url
+      redirect_to home_url
     else
       render :action => :new
     end
