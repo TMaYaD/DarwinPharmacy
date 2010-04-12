72de033f2f2789607202743421ec4097791052b3
Subhash Chandra <TMaYaD@loonyb.in>
2010-04-12 15:36:46 +0530

    purchase bill item sets the rate and vat on product batch

--- test/factories/product_batches.rb
+++ test/factories/product_batches.rb
@@ -4,6 +4,4 @@ Factory.define :product_batch do |p|
   p.pack 1
   p.exp_date Date.today>>12
   p.mrp 72.43
-  p.rate 24.54
-  p.vat 4
 end
--- test/unit/purchase_bill_item_test.rb
+++ test/unit/purchase_bill_item_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class PurchaseBillItemTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "purchase bill item sets rate and vat for product batch on create" do
+    Factory(:franchise, :name => 'DPPL - Vijayawada')
+    purchase_bill_item = Factory.create(:purchase_bill_item)
+
+    assert_equal purchase_bill_item.vat, purchase_bill_item.product_batch.vat
+    assert_equal purchase_bill_item.rate, purchase_bill_item.product_batch.rate
   end
 end
