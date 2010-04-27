2719afff6f03c261b031b28526f607a33c9489fd
Subhash Chandra <TMaYaD@loonyb.in>
2010-04-28 01:06:06 +0530

    Added validations to invoice item
    renamed invoices factory

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,9 @@
 class InvoiceItem < ActiveRecord::Base
-  attr_accessible :invoice, :product_batch, :sale_quantity, :free_quantity, :discount, :rate, :vat
+  attr_accessible :invoice_id, :product_batch_id, :sale_quantity, :free_quantity, :discount, :rate, :vat
+
+  belongs_to :invoice
+  belongs_to :product_batch
+
+  validates_numericality_of :sale_quantity, :greater_than_or_equal_to => 0
+  validates_presence_of :product_batch_id
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,7 +1,27 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe InvoiceItem do
-  it "should be valid" do
-    InvoiceItem.new.should be_valid
+  it "should create a new instance given valid attributes" do
+    attributes = Factory.build(:invoice_item).attributes
+    InvoiceItem.create!(attributes)
   end
+
+  it "should have non negative numeric sale quantity" do
+    should allow_value(0).for :sale_quantity
+    should allow_value(5).for :sale_quantity
+    should_not allow_value(-5).for :sale_quantity
+    should_not allow_value(nil).for :sale_quantity
+    should_not allow_value('ten').for :sale_quantity
+  end
+
+  it "should have a valid product batch" do
+    attributes = Factory.build(:invoice_item, :product_batch => nil).attributes
+    lambda{
+      InvoiceItem.create!(attributes)
+    }.should raise_exception ActiveRecord::RecordInvalid, "Validation failed: Product batch can't be blank"
+
+  end
+  it "should calculate amount"
+
+  it "should belong to an invoice"
 end
